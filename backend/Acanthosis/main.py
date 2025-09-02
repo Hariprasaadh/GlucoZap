84c9d8a0cc5647f61bfa72edd7438fe1d7f7f038
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-09-02 18:44:07 +0530

    bmi

--- backend/Acanthosis/main.py
+++ backend/Acanthosis/main.py
@@ -51,4 +51,4 @@ async def predict(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("yolo_fastapi:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
