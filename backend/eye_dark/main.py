c5eee977f5c76fbca1fc7261b9bd718eaf862549
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-09-03 01:21:10 +0530

    eye_dark fix

--- backend/eye_dark/main.py
+++ backend/eye_dark/main.py
@@ -291,4 +291,4 @@ async def detect_dark_circles(file: UploadFile = File(...)):
     )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
